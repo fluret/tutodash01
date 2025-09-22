5c6872b1970c2f5489a757bb62ae1d52104881d7
Frédéric LURET <frederic.luret@univ-tours.fr>
2025-09-22 16:06:28 +0200

    Ajout layout v1

--- app.py
+++ app.py
@@ -9,3 +9,40 @@ pd.read_csv("avocado.csv")
 )
 
 app = Dash(__name__)
+
+
+app.layout = html.Div(
+    children=[
+        html.H1(children="Avocado Analytics"),
+        html.P(
+            children=(
+                "Analyze the behavior of avocado prices and the number"
+                " of avocados sold in the US between 2015 and 2018"
+            ),
+        ),
+        dcc.Graph(
+            figure={
+                "data": [
+                    {
+                        "x": data["Date"],
+                        "y": data["AveragePrice"],
+                        "type": "lines",
+                    },
+                ],
+                "layout": {"title": "Average Price of Avocados"},
+            },
+        ),
+        dcc.Graph(
+            figure={
+                "data": [
+                    {
+                        "x": data["Date"],
+                        "y": data["Total Volume"],
+                        "type": "lines",
+                    },
+                ],
+                "layout": {"title": "Avocados Sold"},
+            },
+        ),
+    ]
+)
