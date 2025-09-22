d9d9c35f418809846e04467fc35cd6ebeb2d6a14
Frédéric LURET <frederic.luret@univ-tours.fr>
2025-09-22 16:31:28 +0200

    Ajout styles : - favicon - styles CSS - font google

--- app.py
+++ app.py
@@ -9,7 +9,17 @@ pd.read_csv("avocado.csv")
 .sort_values(by="Date")
 )
 
-app = Dash(__name__)
+external_stylesheets = [
+    {
+        "href": (
+            "https://fonts.googleapis.com/css2?"
+            "family=Lato:wght@400;700&display=swap"
+        ),
+        "rel": "stylesheet",
+    },
+]
+app = Dash(__name__, external_stylesheets=external_stylesheets)
+app.title = "Avocado Analytics: Understand Your Avocados!"
 
 
 
