d9a76899a95a3b30853d883933c96ed645b77c26
Frédéric LURET <frederic.luret@univ-tours.fr>
2025-09-23 10:33:03 +0200

    Définition du callback

--- src/cllbacks.py
+++ src/cllbacks.py
@@ -1,4 +1,13 @@
 from dash import Input, Output
 
 def register_callbacks(app, data):
-    # Code du callback
+    @app.callback(
+        Output("price-chart", "figure"),
+        Output("volume-chart", "figure"),
+        Input("region-filter", "value"),
+        Input("type-filter", "value"),
+        Input("date-range", "start_date"),
+        Input("date-range", "end_date"),
+    )
+    def update_charts(region, avocado_type, start_date, end_date):
+        # ...
